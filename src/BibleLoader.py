a6a6a114d88c9077f6a2b4b5d710fe315ec112f0
Olivier Kes <olivier.keshavjee@gmail.com>
2013-06-15 13:04:07 +0200

    Adds BibleLoader, and populates comboBoxes.

---
+++ src/BibleLoader.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+import os
+from PyQt4.QtCore import QRegExp
+
+class BibleLoader():
+
+    def __init__(self, bible):
+
+        self._bible = bible
+        self._books = [0]*27
+        self.loadBible()
+
+
+    def loadBible(self):
+        try:
+            files = os.listdir("../mss/" + self._bible)
+        except FileNotFoundError:
+            print("Please do not enter an imaginary bible.")
+            return
+
+        for i in files:
+            f = open("../mss/" + self._bible + "/" + i, 'r')
+            book = f.read()
+            f.close()
+
+            bookNumber = self.bookcodeToIndex(i)
+            self._books[bookNumber] = self.parseBook(book)
+
+    def parseBook(self, book):
+        """
+        Takes a raw book and returns an array of array of array such as:
+        [0][0][0] = Mt 1, 1
+        [1][8][4] = Mr 9, 5
+        etc.
+        """
+        result = []
+        line = book.split("\n")
+
+        # Puts each verse on one line
+        verse = []
+        v = ""
+        for l in line:
+            if l[0:5] == "     ":
+                if v: verse.append(v)
+                v = l[5:]
+            else:
+                v += l
+        if v: verse.append(v)
+
+        r = QRegExp(r'(\d+):(\d+)\s*(.*)')
+        chapter = []
+        for v in verse:
+            pos = r.indexIn(v)
+            chap = int(r.cap(1))
+            ver = int(r.cap(2))
+            text = r.cap(3)
+            if ver == 1:
+                if len(chapter) > 0: result.append(chapter)
+                chapter = [ver]
+            else:
+                chapter.append(text)
+        if len(chapter) > 0: result.append(chapter)
+
+        return result
+
+    def getBook(self, bookNumber):
+        return self._books[bookNumber]
+
+    def getChapter(self, bookNumber, chapterNumber):
+        return self._books[bookNumber][chapterNumber]
+
+    def getverse(self, bookNumber, chapterNumber, verseNumber):
+        return self._books[bookNumber][chapterNumber][verseNumber]
+
+    def chapterNumber(self, bookNumber):
+        return len(self._books[bookNumber])
+
+    def verseNumber(self, bookNumber, chapterNumber):
+        return len(self._books[bookNumber][chapterNumber])
+
+    def bookcodeToIndex(self, bookcode):
+        """
+        Returns the index of the given bookcode.
+        Exemple: "MT" or "MT.BP5" returns 0 (first book of the NT)
+        """
+        bookorder = ["MT", "MR", "LU", "JOH", "AC", "RO", "1CO", "2CO", "GA",
+                     "EPH", "COL", "PHP", "1TH", "2TH", "1TI", "2TI", "TIT",
+                     "PHM", "HEB", "JAS", "1PE", "2PE", "1JO", "2JO", "3JO",
+                     "JUDE", "RE"]
+
+        for i in bookorder:
+            if i in bookcode: return bookorder.index(i)
+
+        print("ERROR: the bookcode is invalid.")
+
+    def setBible(self, bible):
+        self._bible = bible
+
+    def Bible(self):
+        return self._bible
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -3,6 +3,7 @@
 from PyQt4.QtGui import QMainWindow, QPushButton
 from ui.MainWindow import Ui_MainWindow
 from Game import Game
+from BibleLoader import BibleLoader
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -16,13 +17,51 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Hide some stuff
         self.pushButton_moreOptions.setChecked(False)
 
+        # Loads Bible
+        self.BibleLoader = BibleLoader("BP05FNL")
+
+        # Signal / Slots connections
+        self.comboBox_book.currentIndexChanged.connect(self.populatesChapter)
+        self.comboBox_chapter.currentIndexChanged.connect(self.populatesVerseFrom)
+        self.comboBox_verseFrom.currentIndexChanged.connect(self.populatesVerseTo)
+
+        # Initalisation
         self.loadGame()
+        self.populatesBook()
 
         # Display
         self.show()
 
     def loadGame(self):
+        "Creates and sets the central widget"
         self.game = Game()
         self.setCentralWidget(self.game)
 
+    def populatesBook(self):
+        "Populates the combobox with the NT books names."
+        bookList = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans",
+                "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians",
+                "Philippians", "Colossians", "1 Thessalonians",
+                "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus",
+                "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John",
+                "2 John", "3 John", "Jude", "Revelation"]
+
+        for i in bookList:
+            self.comboBox_book.addItem(i)
+
+    def populatesChapter(self, bookNumber):
+        self.comboBox_chapter.clear()
+        for i in range(self.BibleLoader.chapterNumber(bookNumber)):
+            self.comboBox_chapter.addItem(str(i + 1))
+
+    def populatesVerseFrom(self, chapterNumber):
+        self.comboBox_verseFrom.clear()
+        for i in range(self.BibleLoader.verseNumber(self.comboBox_book.currentIndex(), chapterNumber)):
+            self.comboBox_verseFrom.addItem(str(i + 1))
+
+
+    def populatesVerseTo(self, verseNumber):
+        self.comboBox_verseTo.clear()
+        for i in range(verseNumber, self.BibleLoader.verseNumber(self.comboBox_book.currentIndex(), self.comboBox_chapter.currentIndex())):
+            self.comboBox_verseTo.addItem(str(i + 1))
 
